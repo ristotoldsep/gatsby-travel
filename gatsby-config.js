6ae6b6b2fcb6f14d1d9b1eb103f144bd32575788
Risto Tõldsep <r.toldsep@gmail.com>
2022-09-26 19:55:58 +0300

    Created header and Hero section components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Travel Website`,
+    description: `Travel Website Built with Gatsby (React.js + GraphQL)`,
+    author: `@ristotoldsep`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
